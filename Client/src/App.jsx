e4035f17e6cf75cbc13093d84314b4acfbb6c3e4
Khurram05 <ks032638@gmail.com>
2025-05-06 00:12:57 +0500

    Signup page is created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./Modules/Auth/Pages/Signup";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <div>Openverse APP</div>
-  )
-}
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Routes>
+        <Route element={<Signup />} path="/" />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
