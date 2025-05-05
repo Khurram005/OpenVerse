c516d7655c0ee4ce34c21599e52de47c6279e0d8
Khurram05 <ks032638@gmail.com>
2025-05-05 19:49:39 +0500

    get all search history api created

--- Server/src/Controller/SearchHistoryController/SearchHistoryController.js
+++ Server/src/Controller/SearchHistoryController/SearchHistoryController.js
@@ -1,19 +1,33 @@
 import SearchHistoryService from "../../Services/SearchHistoryService/SearchHistoryService.js";
 
 const SearchHistoryController = {
-    saveSearchHistory: async (req, res) => {
-        try {
-          const searchHistory = await SearchHistoryService.saveSearchHistory(req);
-          res.status(201).json({
-            message: "Search history created successfully",
-            data: searchHistory,
-          });
-        } catch (error) {
-          res
-            .status(error.status || 500)
-            .json({ message: error.message || "Internal server error" });
-        }
-      },
-}
+  saveSearchHistory: async (req, res) => {
+    try {
+      const searchHistory = await SearchHistoryService.saveSearchHistory(req);
+      res.status(201).json({
+        message: "Search history created successfully",
+        data: searchHistory,
+      });
+    } catch (error) {
+      res
+        .status(error.status || 500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  },
 
-export default SearchHistoryController
+  getAllSearchHistory: async (req, res) => {
+    try {
+      const searchHistory = await SearchHistoryService.getAllSearchHistory(req);
+      res.status(200).json({
+        message: "Search history fetched successfully",
+        data: searchHistory,
+      });
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        message: error.message || "Internal server error",
+      });
+    }
+  },
+};
+
+export default SearchHistoryController;
